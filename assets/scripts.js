e327eededc2bedf3f8732f2ed6d8fb6c776d5467
Lauren Hooper <lluther921@gmail.com>
2020-08-04 18:29:52 -0700

    storage working properly

--- assets/scripts.js
+++ assets/scripts.js
@@ -40,8 +40,8 @@ hours.forEach(function (hour) {
  type: 'Text',})
  inputDiv.addClass('col-6');
  inputDiv.attr('id', 'taskHour' + hour);
- const userInput = $('input').val();
- inputDiv.text(userInput);
+ inputDiv.val(localStorage.getItem("taskHour" + hour));
+ 
  
  
  // append to row div
@@ -68,31 +68,8 @@ hours.forEach(function (hour) {
 //  click event for save button
     $(".saveBtn").on('click',function(event){
          event.preventDefault();
-        
-         localStorage.setItem("9am",(taskHour9.value));
-         localStorage.setItem("10am",taskHour10.value);
-         localStorage.setItem("11am",taskHour11.value);
-         localStorage.setItem("12am",taskHour12.value);
-         localStorage.setItem("1pm",taskHour13.value);
-         localStorage.setItem("2pm",taskHour14.value);
-         localStorage.setItem("3pm",taskHour15.value);
-         localStorage.setItem("4pm",taskHour16.value);
-         localStorage.setItem("5pm",taskHour17.value);
+        var inputText = $(event.target).siblings("input")
+        console.log(inputText.val());
+        localStorage.setItem(inputText.attr("id"),inputText.val())
+    
         })
-
-  function init() {
-      
-    document.getElementById("taskHour9").innerHTML =localStorage.getItem("9am");
-           
-            // localStorage.getItem("10am");
-            // localStorage.getItem("11am");
-            // localStorage.getItem("12am");
-            // localStorage.getItem("1pm");
-            // localStorage.getItem("2pm");
-            // localStorage.getItem("3pm");
-            // localStorage.getItem("4pm");
-            // localStorage.getItem("5pm");
-            
-        };
-
-        init();
